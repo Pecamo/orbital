36e769fa707e25c04058bb9fbd677184d221ea53
Kewin Dousse <me@protectator.ch>
2019-08-20 15:59:00 +0200

    Things

---
+++ server/server.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import expressWsWrapper from 'express-ws';
+
+const app = express();
+const expressWs = expressWsWrapper(app);
+
+app.use(function (req, res, next) {
+  console.log('middleware');
+  req.testing = 'testing';
+  return next();
+});
+
+app.use('/static', express.static('public'));
+
+app.get('/', function(req, res, next){
+  console.log('get route', req.testing);
+  res.end();
+});
+
+app.ws('/', function(ws, req) {
+  ws.on('message', function(msg) {
+    console.log(msg);
+  });
+  console.log('socket', req.testing);
+});
+
+app.listen(3000);
