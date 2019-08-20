59fa267a36edafe22f5c0f11e26fae11a01d670e
Sacha Bron <me@sachabron.ch>
2019-08-20 13:56:01 +0200

    refactor player class

--- server/player.ts
+++ server/player.ts
@@ -1,3 +1,10 @@
-class Player {
-    constructor(public color: Color, public x: number, public speed: number, public alive: number) {}
+export class Player {
+    constructor(
+        public color: Color,
+        public x: number,
+        public speed: number = 1,
+        public alive: boolean = true,
+        public faceRight: boolean = true,
+        public shotCooldown: number = 1,
+    ) {}
 }
