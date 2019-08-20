eee9d40f9ad6255b64a3b9691e5c377a8eff00ef
Sacha Bron <me@sachabron.ch>
2019-08-20 13:55:23 +0200

    Add display manager

---
+++ server/display.ts
@@ -0,0 +1,31 @@
+import { Color } from "./color";
+
+export class Display {
+    public drawDot(index: number, color: Color): void {
+        // TODO implement
+    }
+
+    public drawLine(from: number, to: number, color: Color): void {
+        // TODO implement
+    }
+
+    public drawGradient(from: number, to: number, colorFrom: Color, colorTo: Color): void {
+        // TODO implement
+    }
+
+    public drawAll(color: Color): void {
+        // TODO implement
+    }
+
+    public get(index: number): Color {
+        // TODO implement
+    }
+
+    public screenshot(): Color[] {
+        // TODO implement
+    }
+
+    public clear(): void {
+        // TODO implement
+    }
+}
