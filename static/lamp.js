ed1231e185603952515dc675307a698c161fc81d
Sacha Bron <me@sachabron.ch>
2021-07-30 01:02:11 +0200

    First version with fixed colors

---
+++ static/lamp.js
@@ -0,0 +1,19 @@
+const colorPicker = document.getElementById("color-picker");
+colorPicker.addEventListener('input', () => {
+    const hexColor = colorPicker.value;
+    const color = {
+        r: parseInt(hexColor.substring(1, 3), 16),
+        g: parseInt(hexColor.substring(3, 5), 16),
+        b: parseInt(hexColor.substring(5, 7), 16),
+        w: 0,
+    }
+
+    sendColor(color)
+});
+
+function sendColor(color) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/lamp/color", true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify(color));
+}
