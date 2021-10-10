2c20cd4aa4635632e3ff78da845349943e677f1d
Sacha Bron <me@sachabron.ch>
2021-10-11 00:44:55 +0200

    Init Fire Animation

--- server/lamp/index.ts
+++ server/lamp/index.ts
@@ -4,7 +4,8 @@ import { NB_LED, State, state, display } from '../server';
 import { Color } from '../color';
 import { HtmlColors } from '../htmlColors';
 import * as convert from 'color-convert';
-import * as env from "../env";
+import * as env from '../env';
+import * as ct from 'color-temperature';
 
 export const lamp = express();
 lamp.use(express.json());
@@ -14,6 +15,7 @@ enum Animation {
     NONE = "none",
     STROBE = "strobe",
     RAINBOW = "rainbow",
+    FIRE = "fire",
 }
 
 let currentAnimation: Animation = Animation.NONE;
@@ -81,6 +83,13 @@ function startLamp() {
                     display.drawDot(n, color);
                 }
                 break;
+            case Animation.FIRE:
+                for (let n = 0; n < NB_LED / 2; n++) {
+                    const color = temperatureToRgb(n * (2000 / (NB_LED / 2)));
+                    display.drawDot(n + NB_LED / 2, color);
+                    display.drawDot(NB_LED / 2 - n - 1, color);
+                }
+                break;
         }
 
         display.render();
@@ -94,5 +103,26 @@ function startLamp() {
         } else {
             isLampRunning = false;
         }
+    }
+}
+
+function temperatureToRgb(temperature): Color {
+    // Assume temperature in Kelvin
+    // 1st step: too cold to glow (theorically 798K (Draper point), but it looks quite better like this)
+    // 2nd step: the lib returns NaN or 0 for green under 652K
+    // 3rd step: starts to emit green ligth, we can count on the lib
+    const steps = [200, 652, 1000];
+    if (temperature < steps[0]) {
+        return HtmlColors.black;
+    } else if (temperature < steps[1]) {
+        const red = Math.round((temperature - steps[0]) * (255 / (steps[2] - steps[0])));
+        return new Color(red, 0, 0);
+    } else if (temperature < steps[2]) {
+        const red = Math.round((temperature - steps[0]) * (255 / (steps[2] - steps[0])));
+        const { green, blue } = ct.colorTemperature2rgb(temperature);
+        return new Color(red, green, blue);
+    } else {
+        const { red, green, blue } = ct.colorTemperature2rgb(temperature);
+        return new Color(red, green, blue);
     }
 }
