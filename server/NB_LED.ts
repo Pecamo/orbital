8fe33f7dfb082dc9e76203cd3658b3ad54f25573
Sacha Bron <me@sachabron.ch>
2021-12-07 23:14:07 +0100

    Fix config read from LED Server

--- server/NB_LED.ts
+++ server/NB_LED.ts
@@ -4,5 +4,10 @@ import { env } from 'process';
 export let NB_LED: number = null;
 
 export async function fetchNB_LED(): Promise<void> {
-    NB_LED = await axios.get(`http://${env.DISPLAY_API_HOSTNAME}:${env.DISPLAY_API_PORT}/config`);
+    const res = await axios.get(`http://${env.DISPLAY_API_HOSTNAME}:${env.DISPLAY_API_PORT}/config`);
+    NB_LED = parseInt(res.data.NB_LED);
+
+    if (typeof NB_LED !== 'number' || isNaN(NB_LED)) {
+        throw new Error(`NB_LED fetched from http://${env.DISPLAY_API_HOSTNAME}:${env.DISPLAY_API_PORT}/config is not a number`);
+    }
 }
