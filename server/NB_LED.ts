62387bb07a3058f7cba04551a03a946ada4c3912
Sacha Bron <me@sachabron.ch>
2024-08-14 00:05:31 +0200

    Allow to start Orbital server with the Display API

--- server/NB_LED.ts
+++ server/NB_LED.ts
@@ -5,14 +5,21 @@ export let NB_LED: number = null;
 
 export async function fetchNB_LED(): Promise<void> {
   const endpoint = env.USE_WLED ? 'json' : 'config';
-  const res = await axios.get(
-    `http://${env.DISPLAY_API_HOSTNAME}:${env.DISPLAY_API_INFO_PORT}/${endpoint}`
-  );
-  NB_LED = env.USE_WLED ? parseInt(res.data.info.leds.count) : parseInt(res.data.NB_LED);
-
-  if (typeof NB_LED !== "number" || isNaN(NB_LED)) {
-    throw new Error(
-      `NB_LED fetched from http://${env.DISPLAY_API_HOSTNAME}:${env.DISPLAY_API_INFO_PORT}/${endpoint} is not a number`
+  try {
+    const res = await axios.get(
+      `http://${env.DISPLAY_API_HOSTNAME}:${env.DISPLAY_API_INFO_PORT}/${endpoint}`
     );
+    NB_LED = env.USE_WLED ? parseInt(res.data.info.leds.count) : parseInt(res.data.NB_LED);
+
+    if (typeof NB_LED !== "number" || isNaN(NB_LED)) {
+      throw new Error(
+        `NB_LED fetched from http://${env.DISPLAY_API_HOSTNAME}:${env.DISPLAY_API_INFO_PORT}/${endpoint} is not a number`
+      );
+    }
+  } catch (e) {
+    console.error("Error while fetching NB_LED. Please make sure the display API is running.");
+    console.error(e);
+    console.log("Falling back to default value");
+    NB_LED = 60;
   }
 }
